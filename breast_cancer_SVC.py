1c42e194440baf3adf406ac395b03542c6bcfb76
rawatshubham09 <65785009+rawatshubham09@users.noreply.github.com>
2020-06-02 23:56:28 +0530

    Add files via upload

---
+++ breast_cancer_SVC.py
@@ -0,0 +1,50 @@
+# Logistic Regression
+
+# Importing the libraries
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('breast_cancer.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state = 0)
+classifier.fit(X_train, y_train)
+
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+
+
+#printing output
+print("Accuracy : {:.2f} %".format(accuracies.mean()*100))
+print("Standard Davation : {:.2f} %".format(accuracies.std()*100))
+
+#Accuracy = 97.6% & STD = 2.21%
+
+#After Tuning
+# Applying Grid Search to find the best model and the best parameters
+from sklearn.model_selection import GridSearchCV
+parameters = [{'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+              {'C': [1, 10, 100, 1000], 'kernel': ['rbf'], 'gamma': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]}]
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10,
+                           n_jobs = -1)
+grid_search = grid_search.fit(X_train, y_train)
+best_accuracy = grid_search.best_score_
+best_parameters = grid_search.best_params_
+
+#best parameter (C= 1 , kernel = linear)
---
+++ breast_cancer_log.py
@@ -0,0 +1,29 @@
+# Logistic Regression
+
+# Importing the libraries
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('breast_cancer.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+print("Accuracy : {:.2f} %".format(accuracies.mean()*100))
+print("Standard Davation : {:.2f} %".format(accuracies.std()*100))
